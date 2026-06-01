bedbaebdfb46799a7d2e87d6da0867d4fab26be9
Nazmus Sadat <prosadat505@gmail.com>
2026-06-01 10:17:03 +0600

    fix one bug

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # light-kit.js
 This is a JavaScript library that makes coding easier and it made by TypeScript. This is also a frontend library. This library is makes for simple code. The JavaScript function are very simple at there.
 
-# Features
+## Features
 The unique part is type safety and no compilation.
 
 1. Math functions 
 2. String functions
 3. Array functions
 
-# Installation
+## Usage
+```
+// import math functions from light-kit js 
+import { lightMath } from "light-kitjs";
+
+// the arguments are type, minimum and maximum number.
+const num = lightMath.randNum("int", 1, 15);
+
+console.log(num);
+
+```
+
+## Installation
 ```
 npm i light-kitjs 
 ```
--- src/math.ts
+++ src/math.ts
@@ -1,7 +1,28 @@
 // math random function
+type RandomType = "int" | "floor";
 
-export function random(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+export function random(type: RandomType ,min: number, max: number): number {
+  
+  // if min greater than max 
+  if (min > max) {
+    throw new RangeError("Invalid range: minmun number can't greater than maximum number.");
+  }
+
+  // if input values aren't number
+  if (Number.isNaN(min) || Number.isNaN(max)) {
+    throw new TypeError("Invalid input: argument must be a number.")
+  }
+
+
+  switch (type) {
+    case "int": // integer random value
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+      break;
+  
+    case "floor": // floor random value
+      return Math.random() * (max - min) + mim;
+      break;
+  }
 }
 
 
